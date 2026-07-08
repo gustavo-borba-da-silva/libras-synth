32db1c881a26b7a3f67087e4c30f4fa433d24d3a
Gustavo Borba <g237965@dac.unicamp.br>
2026-07-08 17:49:14 -0300

    feat: parseXML and begun new json formation using elan data

--- src/utils/fbxToGlossQuaternions.py
+++ src/utils/fbxToGlossQuaternions.py
@@ -2,11 +2,8 @@ import bpy
 import os
 import json
 from pathlib import Path
-import tkinter as tk
-from tkinter import filedialog
-
-root = tk.Tk()
-root.withdraw()
+import xml.etree.ElementTree as ET
+import math
 
 fbxFile = Path(__file__).parent.parent /"raw_dictionary"/"clara_RU_salada_067_Motion.Fbx"
 file_path = filedialog.askopenfilename(
@@ -25,14 +22,86 @@ scene = bpy.context.scene
 
 ##### frame cropping #######
 
+
+
+
+# Structure:
+#     word/sentence
+#      -> gesture_type                
+#          -> frame_interval
+
+
+
+
 # ELAN DATA GOES HERE
 
 
 
+def parseXML(xmlFile):
+    tree = ET.parse(xmlFile)
+    root = tree.getroot()
 
-# frame_start = 148  
-# frame_end = 202
-# name = "LARANJA"
+    header = root.find("HEADER")
+    time_unit = header.get("TIME_UNITS")
+    time_multiplier = 1
+    frame_rate = 60
+
+    match time_unit:
+        case "milliseconds":
+            time_multiplier = 10**(-3)
+        case "seconds":
+            time_multiplier = 1
+        case "minutes":
+            time_multiplier = 60    
+    
+    framestamps = {}
+    
+    for item in root.findall("TIME_SLOT"):
+        # Ex: framestamps[ts3] = 310 * 10**-3 * 60 = 18,6 -> round(18,6) = 19 (19th frame)
+        framestamps[item.get("TIME_SLOT_ID")] = round(item.get("TIME_VALUE") * time_multiplier * frame_rate)
+
+    # A partir daqui eu já tenho o valor em frames que cada timestamp representa
+    gesture_data = {}
+
+    gloss_data = {}
+
+    for item in root.findall("TIER"):
+
+        annotations = item.findall("ALIGNABLE_ANNOTATION") 
+        if len(annotations) > 0:
+            for ann in annotations:
+                frame_interval = []
+                frame_interval.append(framestamps[ann.get("TIME_SLOT_REF1")])
+                frame_interval.append(framestamps[ann.get("TIME_SLOT_REF2")])
+
+                gesture_data[item.get("TIER_ID")] = frame_interval
+                
+                gloss = ann.find("ANNOTATION_VALUE")
+                gloss_data[gloss.text] = gesture_data
+
+    # A partir daqui temos por exemplo gloss_data["SALADA"] = {"Sinal Lexical MD": [100, 310], "Sinal Lexical ME": [100, 310]}
+
+    return gloss_data
+
+
+parent_folder_path = Path(__file__).parent
+directory = os.fsencode(parent_folder_path)
+gloss_data = {}
+
+for file in os.listdir(directory):
+    filename = os.fsdecode(file)
+    if filename.endswith(".eaf"):
+        gloss_data = parseXML(filename)
+        
+# A partir daqui, tenho gloss_data no global pra usar
+
+for gloss, gesture_data in gloss_data.items():
+    for gesture_type, frame_interval in gesture_data.items():
+        frame_interval = 1        
+
+# AJUSTAR AQUI EM CIMA
+
+
 
 # total_frames = frame_end - frame_start + 1
 
